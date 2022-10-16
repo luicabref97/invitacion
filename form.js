76ea3ee85227c4fd6066ce1297618343803e428b
Luis Cabrera <luiscabreraf2@gmail.com>
2022-10-16 02:16:34 -0300

    se agregaron funcionalidades

--- form.js
+++ form.js
@@ -1,3 +1,23 @@
+const asist = document.getElementById('asist')
+const checkAsistire = document.getElementById('asiste')
+const checkNoAsistire = document.getElementById('noAsiste')
+const nota = document.getElementById('nota')
+
+asist.addEventListener('click', e => {
+    if(checkAsistire.checked) {
+        nota.innerHTML = 'Nota: Si tienes hijos, con tu confirmacion de asistencia estarás confirmando la de ellos'
+    } else if(checkNoAsistire.checked) {
+        nota.innerHTML = ''
+    } else {
+        nota.innerHTML = ''
+    }
+})
+    
+
+// if (checkAsistire.change) {
+//     console.log('Si tienes hijos, con tu confirmacion de asistencia estarás confirmando la de ellos')
+// }
+
 const $form = document.querySelector('#form')
 
 $form.addEventListener('submit', handleSubmit)
@@ -17,3 +37,6 @@ async function handleSubmit(event) {
         alert('Gracias por completar el formulario')
     }
 }
+
+
+
